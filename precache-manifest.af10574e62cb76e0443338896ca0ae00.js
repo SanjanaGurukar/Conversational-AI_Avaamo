776bc6477fd8349b817c41f454841d7de42f068e
Sanjana <sanjana.shambulingappa@mcplc.com>
2020-05-31 20:19:16 +0530

    Updates

---
+++ precache-manifest.af10574e62cb76e0443338896ca0ae00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c78cada8da8b6b73f7c8dbdb840c9969",
+    "url": "/Conversational-AI_Avaamo/index.html"
+  },
+  {
+    "revision": "7123a57ef729135cc547",
+    "url": "/Conversational-AI_Avaamo/static/css/main.6c2f506b.chunk.css"
+  },
+  {
+    "revision": "00fcca3ec42227361113",
+    "url": "/Conversational-AI_Avaamo/static/js/2.465df07d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Conversational-AI_Avaamo/static/js/2.465df07d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7123a57ef729135cc547",
+    "url": "/Conversational-AI_Avaamo/static/js/main.6616f726.chunk.js"
+  },
+  {
+    "revision": "c04f5de8f09fcdacf2e2",
+    "url": "/Conversational-AI_Avaamo/static/js/runtime-main.043ecafd.js"
+  }
+]);
